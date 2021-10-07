7d22cda1783f7338fb52e86550ae074a6f709108
unknown <jahinhasan48@gmail.com>
2021-10-07 13:43:04 +0600

    jisan

---
+++ index.js
@@ -0,0 +1,46 @@
+function startFan() {
+    console.log("walk towards the switch")
+    console.log('press the switch');
+}
+
+startFan();
+
+/*
+function StartFan(taka) {
+    console.log("walk towards the switch");
+    console.log('press the switch');
+    var singraprice = 10;
+    var singraQuantity = taka / singraprice;
+    return singraQuantity;
+}
+
+var money = 90;
+var singra = StartFan(money);
+console.log('Ai nen singra', singra);
+*/
+
+/*
+
+function StartFan(taka) {
+    console.log("walk towards the switch");
+    console.log('press the switch');
+    var singraprice = 10;
+    var singraQuantity = taka * singraprice;
+    return singraQuantity;
+}
+
+var money = 90;
+var singra = StartFan(money);
+console.log('Ai nen singra', singra);
+
+*/
+
+
+//  Objects Properties
+var book = {
+    title: 'BitBox',
+    author: 'Simon Sinek',
+    price: 200,
+    pages: 346,
+    publisher: 'penguin'
+};
